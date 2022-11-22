3522b0f3c7ca7cb880b5babb8ba9b9a48fb885f0
kapteinjoel <73760483+kapteinjoel@users.noreply.github.com>
2022-11-21 20:28:37 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,5 @@ from discord.ext import commands
 
 #static setup related variables
 CLASSES = [music]
-TOKEN = os.environ['TOKEN']
 INTENTS, INTENTS.members = (discord.Intents.all(), True)
 CLIENT = commands.Bot(intents=INTENTS, command_prefix='>')
